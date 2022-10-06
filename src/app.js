8203a3cdb94a69c2faa8b359aa53dc16799977fd
marcelrm11 <marcelrm11@gmail.com>
2022-10-06 15:39:19 +0000

    finished exercise

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let part1 = who[Math.floor(Math.random() * who.length)];
+  let part2 = action[Math.floor(Math.random() * action.length)];
+  let part3 = what[Math.floor(Math.random() * what.length)];
+  let part4 = when[Math.floor(Math.random() * when.length)];
+  let sentence = `${part1} ${part2} ${part3} ${part4}.`;
+  document.getElementById("excuse").innerHTML = sentence;
 };
